ec29ac25cdd444c9b4553884602553d02482cbcb
dyf189 <251273640@qq.com>
2025-10-18 08:53:33 +0800

    尝试在官网直接集成下载

---
+++ download/config/file.js
@@ -0,0 +1,55 @@
+// 数据config
+
+const files = [
+    {
+        name: "USF V3-1-2-B&F",
+        description: "指令分为/usf:manager、/usf:func，供管理员、成员使用",
+        changelog: 
+       [
+                "暂指令分为/usf:manager、/usf:func，供管理员、成员使用"
+       ],
+        contributors: [
+            "USFrameTeam"
+        ],
+        supportVersions: ["1.21.94"],
+        releaseDate: "2025-8-1",
+        tags: ["测试版", "1.21"],
+        downloadLink: "/download/files/main/无名氏插件-3-1-2-B&F(1).mcaddon",
+        feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
+        urgentUpdate: false  // 标记为紧急更新 
+       },
+       {
+        name: "USF V3-1-1-B",
+        description: "优化领地边框显示，添加全局领地管理功能",
+        changelog: 
+       [
+                "优化领地边框显示，添加全局领地管理功能"
+       ],
+        contributors: [
+            "USFrameTeam"
+        ],
+        supportVersions: ["1.21.94"],
+        releaseDate: "2025-8-1",
+        tags: ["测试版", "1.21"],
+        downloadLink: "/download/files/main/无名氏插件V3-3-1-1(1).mcpack",
+        feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
+        urgentUpdate: false  // 标记为紧急更新 
+       },
+        {
+         name: "USF V3-1-0-B",
+         description: "添加领地",
+         changelog: 
+        [
+                 "添加领地"
+        ],
+         contributors: [
+             "USFrameTeam"
+         ],
+         supportVersions: ["1.21.94"],
+         releaseDate: "2025-7-25",
+         tags: ["测试版", "1.21"],
+         downloadLink: "/download/files/main/无名氏插件3-1-0-B(1).mcpack",
+         feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
+         urgentUpdate: false  // 标记为紧急更新 
+        },
+ ];
