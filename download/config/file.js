dbf83bcab44765b058c21c71ffe030e39033f93b
dyf189 <251273640@qq.com>
2025-10-18 09:05:38 +0800

    更新文件

--- download/config/file.js
+++ download/config/file.js
@@ -2,54 +2,74 @@
 
 const files = [
     {
-        name: "USF V3-1-2-B&F",
-        description: "指令分为/usf:manager、/usf:func，供管理员、成员使用",
-        changelog: 
+        name: "NeoUSF(USFV3)-3.1.8",
+        description: "具体查看USFV3 Github仓库",
+        changelog:
        [
-                "暂指令分为/usf:manager、/usf:func，供管理员、成员使用"
-       ],
-        contributors: [
-            "USFrameTeam"
+                "",
         ],
-        supportVersions: ["1.21.94"],
-        releaseDate: "2025-8-1",
-        tags: ["测试版", "1.21"],
-        downloadLink: "/download/files/main/无名氏插件-3-1-2-B&F(1).mcaddon",
-        feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
-        urgentUpdate: false  // 标记为紧急更新 
-       },
-       {
-        name: "USF V3-1-1-B",
-        description: "优化领地边框显示，添加全局领地管理功能",
-        changelog: 
-       [
-                "优化领地边框显示，添加全局领地管理功能"
-       ],
         contributors: [
-            "USFrameTeam"
+            "USF开发团队"
         ],
-        supportVersions: ["1.21.94"],
-        releaseDate: "2025-8-1",
+        supportVersions: ["1.21.111"],
+        releaseDate: "2025-10-6",
         tags: ["测试版", "1.21"],
-        downloadLink: "/download/files/main/无名氏插件V3-3-1-1(1).mcpack",
+        downloadLink: "/download/files/main/NeoUSF3.1.8(1).zip",
         feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
-        urgentUpdate: false  // 标记为紧急更新 
-       },
+        urgentUpdate: false // 标记为紧急更新
+    },
         {
-         name: "USF V3-1-0-B",
-         description: "添加领地",
-         changelog: 
+         name: "USF0.7.16F",
+         description: "适配1.21.111 修复了日志系统无法使用的问题",
+         changelog:
         [
-                 "添加领地"
-        ],
-         contributors: [
-             "USFrameTeam"
+                 "修复了日志系统无法使用的问题",
          ],
-         supportVersions: ["1.21.94"],
-         releaseDate: "2025-7-25",
+         contributors: [
+             "USF开发团队"
+         ],
+         supportVersions: ["1.21.111"],
+         releaseDate: "2025-10-6",
          tags: ["测试版", "1.21"],
-         downloadLink: "/download/files/main/无名氏插件3-1-0-B(1).mcpack",
+         downloadLink: "/download/files/main/USF0.7.16F.zip",
          feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
-         urgentUpdate: false  // 标记为紧急更新 
-        },
+         urgentUpdate: true // 标记为紧急更新
+     },
+        {
+         name: "USF0.7.15B",
+         description: "适配1.21.111",
+         changelog:
+        [
+                 "兼容1.21.111",
+         ],
+         contributors: [
+             "USF开发团队"
+         ],
+         supportVersions: ["1.21.111"],
+         releaseDate: "2025-9-29",
+         tags: ["测试版", "1.21"],
+         downloadLink: "/download/files/main/USF0.7.15B.zip",
+         feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
+         urgentUpdate: false // 标记为紧急更新
+     },
+        {
+         name: "USF0.7.14C",
+         description: "玩家互传加入了动画传送（默认关闭），可以在插件设置的传送设置调整",
+         changelog:
+        [
+                 "玩家互传加入了动画传送（默认关闭），可以在插件设置的传送设置调整",
+                 "兼容1.21.9x 1.21.10x"
+         ],
+         contributors: [
+             "USF开发团队",
+             "小洋骢"
+         ],
+         supportVersions: ["1.21.90","1.21.92","1.21.93","1.21.94","1.21.100"],
+         releaseDate: "2025-8-13",
+         tags: ["测试版", "1.21"],
+         downloadLink: "/download/files/main/USF0.7.14C.zip",
+         feedbackLink: "https://github.com/YYTZ666/usfdown/issues/new/choose",
+         urgentUpdate: false // 标记为紧急更新
+     },
+
  ];
